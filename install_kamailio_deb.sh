44ca288f93c91c83da7b77d3e4f464a2c2ced27e
bfabien99 <fabienbrou99@gmail.com>
2024-12-02 15:54:18 +0000

    restructuration du script

--- install_kamailio_deb.sh
+++ install_kamailio_deb.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+
+## Vérifier que le script est lancé en mode root
+if [[ $EUID -ne 0 ]]; then
+    echo ":: XX Ce script doit être exécuté avec des privilèges root. Veuillez réessayer avec 'sudo'. XX" >&2
+    echo "## Fin du programme d'installation"
+    exit 1
+fi
+
 ## Installation de Kamailio sur un système Debian
 echo "## Début de l'installation de Kamailio sur votre système $1"
-sleep 1
+sleep 0.5
 
 ## Extraire le nom de code de Debian
 if [[ -f /etc/os-release ]]; then
     codename=$(grep VERSION_CODENAME /etc/os-release | cut -d= -f2)
 else
-    echo "XX Impossible de déterminer le nom de code. Assurez-vous que vous utilisez Debian."
+    echo ":: XX Impossible de déterminer le nom de code. Assurez-vous que vous utilisez Debian. XX"
     echo "## Fin du programme d'installation"
     exit 1
 fi
@@ -17,6 +25,9 @@ sleep 0.5
 echo "## Vérification de l'existence de Kamailio"
 if [[ -x /usr/sbin/kamailio ]] || command -v kamailio >/dev/null 2>&1; then
     echo ":: Kamailio est déjà installé sur ce système."
+    sleep 0.5
+    command kamailio -V
+    sleep 0.5
     echo "## Fin du programme d'installation de Kamailio"
     exit 0
 fi
@@ -26,42 +37,13 @@ echo ":: Kamailio n'est pas installé sur ce système."
 echo "## Installation de Kamailio sur votre distribution..."
 
 sleep 0.5
-## Vérifier que le script est lancé en mode root
-if [[ $EUID -ne 0 ]]; then
-    echo "XX Ce script doit être exécuté avec des privilèges root. Veuillez réessayer avec 'sudo'." >&2
-    echo "## Fin du programme d'installation"
-    exit 1
-fi
-
-sleep 1
-## Installation des dépendances requises
-# Mettre à jour les dépôts APT
-echo "Mise à jour des dépôts APT..."
-if apt update -y; then
-    echo "Mise à jour des dépôts réussie."
-else
-    echo "XX Erreur lors de la mise à jour des dépôts. Vérifiez votre connexion réseau ou vos sources APT." >&2
-    echo "..."
-fi
-
-sleep 0.5
-# Mise à niveau des paquets installés
-echo "Mise à niveau des paquets installés..."
-if apt upgrade -y; then
-    echo "Tous les paquets ont été mis à niveau avec succès."
-else
-    echo "XX Erreur lors de la mise à niveau des paquets. Vérifiez les journaux pour plus de détails." >&2
-    echo "..."
-fi
-
-sleep 0.5
 ## Vérifier si wget est installé, sinon l'installer
 if ! command -v wget >/dev/null 2>&1; then
     echo ":: wget n'est pas installé, installation en cours..."
     if apt install -y wget; then
         echo ":: wget a été installé avec succès."
     else
-        echo ":: L'installation de wget a échoué." >&2
+        echo ":: XX L'installation de wget a échoué. XX" >&2
         echo "## Fin du programme d'installation"
         exit 1
     fi
@@ -69,32 +51,47 @@ else
     echo ":: wget est déjà installé sur ce système."
 fi
 
-sleep 1
+sleep 0.5
 ## Ajout de la clé GPG de Kamailio
-echo "Ajout de la clé GPG de Kamailio..."
+echo "## Ajout de la clé GPG de Kamailio..."
+sleep 0.5
 wget -O- https://deb.kamailio.org/kamailiodebkey.gpg | sudo apt-key add -
 if [[ $? -ne 0 ]]; then
-    echo ":: Échec de l'ajout de la clé GPG de Kamailio." >&2
+    echo ":: XX Échec de l'ajout de la clé GPG de Kamailio. XX" >&2
     echo "## Fin du programme d'installation"
     exit 1
 fi
 echo ":: Clé GPG de Kamailio ajoutée avec succès."
 
-sleep 1
+sleep 0.5
 ## Ajouter les sources Kamailio à APT
-echo "Ajout des sources Kamailio à votre fichier /etc/apt/sources.list..."
+echo "## Ajout des sources Kamailio à votre fichier /etc/apt/sources.list..."
+sleep 0.5
 echo "deb http://deb.kamailio.org/kamailio58 $codename main" | sudo tee -a /etc/apt/sources.list > /dev/null
 echo "deb-src http://deb.kamailio.org/kamailio58 $codename main" | sudo tee -a /etc/apt/sources.list > /dev/null
 if [[ $? -ne 0 ]]; then
-    echo ":: Échec de l'ajout des sources Kamailio dans /etc/apt/sources.list." >&2
+    echo ":: XX Échec de l'ajout des sources Kamailio dans /etc/apt/sources.list. XX" >&2
     echo "## Fin du programme d'installation"
     exit 1
 fi
 echo ":: Sources Kamailio ajoutées avec succès."
 
-sleep 1
+sleep 0.5
+## Installation des dépendances requises
+# Mettre à jour les dépôts APT
+echo "## Mise à jour des dépôts APT..."
+sleep 0.5
+if apt update -y; then
+    echo ":: Mise à jour des dépôts réussie."
+else
+    echo ":: XX Erreur lors de la mise à jour des dépôts. Vérifiez votre connexion réseau ou vos sources APT. XX" >&2
+    echo "..."
+fi
+
+sleep 0.5
 ## Installation de MySQL Server et Kamailio avec modules MySQL
-echo "Installation de MySQL Server et des modules Kamailio MySQL..."
+echo "## Installation de MySQL Server et des modules Kamailio MySQL..."
+sleep 0.5
 if apt -y install default-mysql-server; then
     echo ":: MySQL Server a été installé avec succès."
 else
@@ -104,7 +101,9 @@ else
 fi
 
 sleep 0.5
-# Installation de Kamailio et des modules MySQL
+## Installation de Kamailio et des modules MySQL
+echo "## Installation de Kamailio et des modules MySQL"
+sleep 0.5
 if apt -y install kamailio kamailio-mysql-modules; then
     echo ":: Kamailio et les modules MySQL ont été installés avec succès."
 else
@@ -114,16 +113,21 @@ else
 fi
 
 sleep 0.5
-# Vérification de l'installation de Kamailio
+## Vérification de l'installation de Kamailio
 if [[ -x /usr/sbin/kamailio ]] || command -v kamailio >/dev/null 2>&1; then
     echo "## Installation terminée."
-    echo ":: Kamailio s'est bien installé sur ce système."
-    echo "## Affichage de la version"
+    sleep 0.5
     command kamailio -V
+    sleep 0.5
+    echo ":: Kamailio s'est bien installé sur ce système."
+    sleep 0.5
     echo "## Démarrage de Kamailio"
+    sleep 0.5
     systemctl enable kamailio
     systemctl start kamailio
+    sleep 0.5
     echo ":: Kamailio a démarré avec succès."
+    sleep 0.5
     echo "## Fin du programme d'installation de Kamailio"
     exit 0
 else
--- install_kamailio_ubuntu.sh
+++ install_kamailio_ubuntu.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+
+## Vérifier que le script est lancé en mode root
+if [[ $EUID -ne 0 ]]; then
+    echo ":: XX Ce script doit être exécuté avec des privilèges root. Veuillez réessayer avec 'sudo'." >&2
+    echo "## Fin du programme d'installation"
+    exit 1
+fi
+
 ## Installation de Kamailio sur un système Ubuntu
 echo "## Début de l'installation de Kamailio sur votre système $1"
-sleep 1
+sleep 0.5
 
 ## Extraire le nom de code d'Ubuntu
 if [[ -f /etc/os-release ]]; then
     codename=$(grep VERSION_CODENAME /etc/os-release | cut -d= -f2)
 else
-    echo "XX Impossible de déterminer le nom de code. Assurez-vous que vous utilisez Ubuntu."
+    echo ":: XX Impossible de déterminer le nom de code. Assurez-vous que vous utilisez Ubuntu."
     echo "## Fin du programme d'installation"
     exit 1
 fi
@@ -15,8 +23,12 @@ fi
 sleep 0.5
 ## Vérifier si Kamailio est déjà installé
 echo "## Vérification de l'existence de Kamailio"
+sleep 0.5
 if [[ -x /usr/sbin/kamailio ]] || command -v kamailio >/dev/null 2>&1; then
     echo ":: Kamailio est déjà installé sur ce système."
+    sleep 0.5
+    command kamailio -V
+    sleep 0.5
     echo "## Fin du programme d'installation de Kamailio"
     exit 0
 fi
@@ -26,21 +38,13 @@ echo ":: Kamailio n'est pas installé sur ce système."
 echo "## Installation de Kamailio sur votre distribution..."
 
 sleep 0.5
-## Vérifier que le script est lancé en mode root
-if [[ $EUID -ne 0 ]]; then
-    echo "XX Ce script doit être exécuté avec des privilèges root. Veuillez réessayer avec 'sudo'." >&2
-    echo "## Fin du programme d'installation"
-    exit 1
-fi
-
-sleep 0.5
 ## Vérifier si wget est installé, sinon l'installer
 if ! command -v wget >/dev/null 2>&1; then
     echo ":: wget n'est pas installé, installation en cours..."
     if apt install -y wget; then
         echo ":: wget a été installé avec succès."
     else
-        echo ":: L'installation de wget a échoué." >&2
+        echo ":: XX L'installation de wget a échoué. XX" >&2
         echo "## Fin du programme d'installation"
         exit 1
     fi
@@ -48,63 +52,70 @@ else
     echo ":: wget est déjà installé sur ce système."
 fi
 
-sleep 1
+sleep 0.5
 ## Installation des dépendences réquises
-echo "Installation des dépendances réquises: gnupg2 mariadb-server curl unzip"
-echo "Installation de gnupg2"
+echo "## Installation des dépendances réquises: gnupg2 mariadb-server curl unzip"
+sleep 0.5
+echo "## Installation de gnupg2"
+sleep 0.5
 if apt -y install gnupg2; then
     echo ":: gnupg2 a été installé avec succès."
 else
-    echo ":: L'installation de gnupg2 a échoué." >&2
+    echo ":: XX L'installation de gnupg2 a échoué. XX" >&2
     echo "## Fin du programme d'installation"
     exit 1
 fi
 
 sleep 0.5
-echo "Installation de mariadb-server"
+echo "## Installation de mariadb-server"
+sleep 0.5
 if apt -y install mariadb-server; then
     echo ":: mariadb-server a été installé avec succès."
 else
-    echo ":: L'installation de mariadb-server a échoué." >&2
+    echo ":: XX L'installation de mariadb-server a échoué. XX" >&2
     echo "## Fin du programme d'installation"
     exit 1
 fi
 
 sleep 0.5
-echo "Installation de curl"
+echo "## Installation de curl"
+sleep 0.5
 if apt -y install curl; then
     echo ":: curl a été installé avec succès."
 else
-    echo ":: L'installation de curl a échoué." >&2
+    echo ":: XX L'installation de curl a échoué. XX" >&2
     echo "## Fin du programme d'installation"
     exit 1
 fi
 
 sleep 0.5
-echo "Installation de unzip"
+echo "## Installation de unzip"
+sleep 0.5
 if apt -y install unzip; then
     echo ":: unzip a été installé avec succès."
 else
-    echo ":: L'installation de unzip a échoué." >&2
+    echo ":: XX L'installation de unzip a échoué. XX" >&2
     echo "## Fin du programme d'installation"
     exit 1
 fi
 
 
-sleep 1
+sleep 0.5
 ## Ajout de la clé GPG de Kamailio
-echo "Ajout de la clé GPG de Kamailio..."
+echo "## Ajout de la clé GPG de Kamailio..."
+sleep 0.5
 wget -O- https://deb.kamailio.org/kamailiodebkey.gpg | apt-key add -
 if [[ $? -ne 0 ]]; then
-    echo ":: Échec de l'ajout de la clé GPG de Kamailio." >&2
+    echo ":: XX Échec de l'ajout de la clé GPG de Kamailio. XX" >&2
     echo "## Fin du programme d'installation"
     exit 1
 fi
 echo ":: Clé GPG de Kamailio ajoutée avec succès."
 
-sleep 1
+sleep 0.5
 ## Ajouter les sources Kamailio à APT
-echo "Ajout des sources Kamailio à votre fichier /etc/apt/sources.list.d/kamailio.list"
+echo "## Ajout des sources Kamailio à votre fichier /etc/apt/sources.list.d/kamailio.list"
+sleep 0.5
 echo "deb http://cz.archive.ubuntu.com/ubuntu $codename main" | sudo tee -a /etc/apt/sources.list.d/kamailio.list > /dev/null
 echo "deb http://deb.kamailio.org/kamailio58 $codename main" | sudo tee -a /etc/apt/sources.list.d/kamailio.list > /dev/null
 echo "deb-src http://deb.kamailio.org/kamailio58 $codename main" | sudo tee -a /etc/apt/sources.list.d/kamailio.list > /dev/null
@@ -115,23 +126,24 @@ if [[ $? -ne 0 ]]; then
 fi
 echo ":: Sources Kamailio ajoutées avec succès."
 
-sleep 1
+sleep 0.5
 ## Installation des dépendances requises
 # Mettre à jour les dépôts APT
-echo "Mise à jour des dépôts APT..."
+echo "## Mise à jour des dépôts APT..."
+sleep 0.5
 if apt update -y; then
     echo "Mise à jour des dépôts réussie."
 else
-    echo "XX Erreur lors de la mise à jour des dépôts. Vérifiez votre connexion réseau ou vos sources APT." >&2
+    echo ":: XX Erreur lors de la mise à jour des dépôts. Vérifiez votre connexion réseau ou vos sources APT. XX" >&2
     echo "... "
 fi
 
-sleep 1
+sleep 0.5
 # Installation de Kamailio et des modules MySQL
 if apt -y install kamailio kamailio-mysql-modules; then
     echo ":: Kamailio et les modules MySQL ont été installés avec succès."
 else
-    echo ":: L'installation de Kamailio et des modules MySQL a échoué." >&2
+    echo ":: XX L'installation de Kamailio et des modules MySQL a échoué. XX" >&2
     echo "## Fin du programme d'installation"
     exit 1
 fi
@@ -140,12 +152,16 @@ sleep 0.5
 # Vérification de l'installation de Kamailio
 if [[ -x /usr/sbin/kamailio ]] || command -v kamailio >/dev/null 2>&1; then
     echo "## Installation terminée."
-    echo ":: Kamailio s'est bien installé sur ce système."
-    echo "## Affichage de la version"
+    sleep 0.5
     command kamailio -V
+    sleep 0.5
+    echo ":: Kamailio s'est bien installé sur ce système."
+    sleep 0.5
     echo "## Démarrage de Kamailio"
+    sleep 0.5
     systemctl enable kamailio
     systemctl start kamailio
+    sleep 0.5
     echo ":: Kamailio a démarré avec succès."
     echo "## Fin du programme d'installation de Kamailio"
     exit 0
