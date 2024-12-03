d692610998aba15df8f88fde67e650c9d31acb1b
bfabien99 <fabienbrou99@gmail.com>
2024-12-03 13:25:18 +0000

    Ajout de l'utilisateur système kamailio pour tester

--- install_from_source.sh
+++ install_from_source.sh
@@ -6,168 +6,88 @@ sleep 0.5
 ## Vérifier que le script est lancé en mode root
 if [[ $EUID -ne 0 ]]; then
     echo ":: XX Ce script doit être exécuté avec des privilèges root. Veuillez réessayer avec 'sudo'." >&2
-    echo "## Fin du programme d'installation"
     exit 1
 fi
 
-# Vérifier la distribution du système
+## Vérifier la distribution
 if [[ -f /etc/os-release ]]; then
     distribution=$(grep ^ID= /etc/os-release | cut -d= -f2 | tr -d '"')
     codename=$(grep VERSION_CODENAME /etc/os-release | cut -d= -f2)
-    echo ":: Detail du système"
-    echo ":: Votre distribution est : $distribution"
-    echo ":: Nom de code : $codename"
+    echo ":: Détails du système : $distribution ($codename)"
 else
     echo ":: XX Impossible de déterminer la distribution. Assurez-vous que votre système utilise /etc/os-release. XX"
     exit 1
 fi
 
-## Installation de Kamailio
-sleep 0.5
-echo "## Installation de Kamailio"
-sleep 0.5
-
 ## Mettre à jour les dépôts APT
 echo "## Mise à jour des dépôts APT..."
 sleep 0.5
-if apt update -y; then
-    echo ":: Mise à jour des dépôts réussie."
-else
-    echo ":: XX Erreur lors de la mise à jour des dépôts. Vérifiez votre connexion réseau ou vos sources APT. XX" >&2
-    echo "..."
-fi
-
-## Installation des dépendances requises
-sleep 0.5
-echo "## Installation des dépendances requises"
-sleep 0.5
-echo "## Installation de make"
-sleep 0.5
-if apt -y install make; then
-    echo ":: make a été installé avec succès."
-else
-    echo ":: XX L'installation de make a échoué. XX" >&2
-    echo "## Fin du programme d'installation"
+if ! apt update -y; then
+    echo ":: XX Échec lors de la mise à jour des dépôts APT. XX" >&2
     exit 1
 fi
 
+## Installation des dépendances
+echo "## Installation des dépendances requises..."
 sleep 0.5
-echo "## Installation de autoconf"
-sleep 0.5
-if apt -y install autoconf; then
-    echo ":: autoconf a été installé avec succès."
+deps=(make autoconf pkg-config git gcc g++ flex bison libssl-dev default-libmysqlclient-dev)
+
+for dep in "${deps[@]}"; do
+    echo "Installation de $dep..."
+    if ! apt install -y "$dep"; then
+        echo ":: XX L'installation de $dep a échoué. XX" >&2
+        exit 1
+    fi
+    sleep 0.5
+done
+
+## Création du dossier de stockage
+echo "## Préparation du dossier pour Kamailio..."
+kamailio_src="/usr/local/src/kamailio-5.8"
+if [[ ! -d $kamailio_src ]]; then
+    mkdir -p "$kamailio_src"
+    echo ":: Dossier créé : $kamailio_src"
 else
-    echo ":: XX L'installation de autoconf a échoué. XX" >&2
-    echo "## Fin du programme d'installation"
-    exit 1
+    echo ":: Dossier déjà existant : $kamailio_src"
 fi
 
-sleep 0.5
-echo "## Installation de pkg-config"
-sleep 0.5
-if apt -y install pkg-config; then
-    echo ":: pkg-config a été installé avec succès."
-else
-    echo ":: XX L'installation de pkg-config a échoué. XX" >&2
-    echo "## Fin du programme d'installation"
+## Clonage du dépôt Git
+echo "## Clonage du dépôt Kamailio dans $kamailio_src..."
+cd "$kamailio_src"
+if ! git clone --depth 1 --branch 5.8 https://github.com/kamailio/kamailio kamailio; then
+    echo ":: XX Échec du clonage du dépôt Git. Vérifiez votre connexion réseau. XX" >&2
     exit 1
 fi
-
-sleep 0.5
-echo "## Installation de git"
-sleep 0.5
-if apt -y install git; then
-    echo ":: git a été installé avec succès."
-else
-    echo ":: XX L'installation de git a échoué. XX" >&2
-    echo "## Fin du programme d'installation"
-    exit 1
-fi
-
-sleep 0.5
-echo "## Installation de gcc"
-sleep 0.5
-if apt -y install gcc; then
-    echo ":: gcc a été installé avec succès."
-else
-    echo ":: XX L'installation de gcc a échoué. XX" >&2
-    echo "## Fin du programme d'installation"
-    exit 1
-fi
-
-sleep 0.5
-echo "## Installation de g++"
-sleep 0.5
-if apt -y install g++; then
-    echo ":: g++ a été installé avec succès."
-else
-    echo ":: XX L'installation de g++ a échoué. XX" >&2
-    echo "## Fin du programme d'installation"
-    exit 1
-fi
-
-sleep 0.5
-echo "## Installation de flex"
-sleep 0.5
-if apt -y install flex; then
-    echo ":: flex a été installé avec succès."
-else
-    echo ":: XX L'installation de flex a échoué. XX" >&2
-    echo "## Fin du programme d'installation"
-    exit 1
-fi
-
-sleep 0.5
-echo "## Installation de bison"
-sleep 0.5
-if apt -y install bison; then
-    echo ":: bison a été installé avec succès."
-else
-    echo ":: XX L'installation de bison a échoué. XX" >&2
-    echo "## Fin du programme d'installation"
-    exit 1
-fi
-
-sleep 0.5
-echo "## Installation de libssl-dev"
-sleep 0.5
-if apt -y install libssl-dev; then
-    echo ":: libssl-dev a été installé avec succès."
-else
-    echo ":: XX L'installation de libssl-dev a échoué. XX" >&2
-    echo "## Fin du programme d'installation"
-    exit 1
-fi
-
-sleep 0.5
-echo "## Installation de default-libmysqlclient-dev"
-sleep 0.5
-if apt -y install default-libmysqlclient-dev; then
-    echo ":: default-libmysqlclient-dev a été installé avec succès."
-else
-    echo ":: XX L'installation de default-libmysqlclient-dev a échoué. XX" >&2
-    echo "## Fin du programme d'installation"
-    exit 1
-fi
-
-## Création du dossier pour stocker kamailio
-sleep 0.5
-echo "## Création du dossier /usr/local/src/kamailio-5.8"
-mkdir -p /usr/local/src/kamailio-5.8
-echo ":: Dossier créer"
-sleep 0.5
-echo "## Clonage du repo git dans /usr/local/src/kamailio-5.8"
-cd /usr/local/src/kamailio-5.8
-git clone --depth 1 --branch 5.8 https://github.com/kamailio/kamailio kamailio
 cd kamailio
-echo ":: clonage réussi"
 
-sleep 0.5
-echo "## ajout du module db_mysql et tls"
-make include_modules="db_mysql tls" cfg
-make all
-make install
-su -l
-make install-systemd-debian
-systemctl enable kamailio
+## Compilation et installation
+echo "## Compilation et installation de Kamailio avec les modules db_mysql et tls..."
+if ! make include_modules="db_mysql tls" cfg || ! make all || ! make install; then
+    echo ":: XX La compilation ou l'installation de Kamailio a échoué. XX" >&2
+    exit 1
+fi
+
+# Vérifier et ajouter l'utilisateur système 'kamailio'
+echo "Ajout de l'utilisateur système kamailio..."
+if /usr/sbin/adduser --system --group --no-create-home kamailio >/dev/null 2>&1; then
+    echo ":: Utilisateur 'kamailio' ajouté avec succès."
+else
+    echo ":: XX Échec avec adduser, tentative avec useradd... XX"
+    if ! /usr/sbin/useradd -r -M -s /bin/false kamailio >/dev/null 2>&1; then
+        echo ":: XX Échec complet lors de l'ajout de l'utilisateur système. XX" >&2
+        exit 1
+    fi
+fi
+
+## Configuration des services systemd
+echo "## Configuration des services systemd pour Kamailio..."
+if ! make install-systemd-debian || ! systemctl enable kamailio; then
+    echo ":: XX La configuration de Kamailio avec systemd a échoué. XX" >&2
+    exit 1
+fi
 systemctl daemon-reload
+
+## Fin
+echo "## Installation et configuration de Kamailio terminées."
+echo ":: Vous pouvez démarrer Kamailio avec 'systemctl start kamailio'."
+exit 0
